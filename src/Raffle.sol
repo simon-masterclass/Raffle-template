4d6f3fdbf2f633dedd237aaa8279e5879022b80c
Simon Ionashku <simon.ionashku@gmail.com>
2024-07-14 23:17:38 -0400

    [14] - Header 134

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -227,7 +227,10 @@ contract Raffle is VRFConsumerBaseV2Plus {
         s_LinkToken.transfer(to, amount);
     }
 
-    /* Getter Functions */
+    /*|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*
+                            Getter Functions 
+     *|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
+
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
@@ -237,23 +240,3 @@ contract Raffle is VRFConsumerBaseV2Plus {
     }
 }
 
-// Layout of Contract:
-// version
-// imports
-// errors
-// interfaces, libraries, contracts
-// Type declarations
-// State variables
-// Events
-// Modifiers
-// Functions
-
-// Layout of Functions:
-// constructor
-// receive function (if exists)
-// fallback function (if exists)
-// external
-// public
-// internal
-// private
-// view & pure functions
